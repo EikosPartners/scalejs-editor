65adea8faf4b745a4e8459caeaf890673b8dee3c
rvianna <rvianna@att.net>
2017-01-18 15:08:46 -0500

    added button controls

--- public/src/app/main/mainBindings.js
+++ public/src/app/main/mainBindings.js
@@ -1,10 +1,27 @@
+import { unwrap } from 'knockout';
+
 export default {
-    'main-split-actions': function(ctx, args) {
+    'main-split-actions': function (ctx, args) {
+        const value = args[1];
+        const section = args[2] || 'main';
+        return {
+            click: () => {
+                this.controls[section](value === 'split' ? value : ['collapse', value]);
+            }
+        }
+    },
+    'main-output-view': function (ctx, args) {
         const value = args[1];
         return {
             click: () => {
-                this.controls.main(value === 'split' ? value : ['collapse', value]);
+                if (value === unwrap(this.controls.output)) { return; }
+                this.controls.output(value);
             }
+        }
+    },
+    'main-iframe-styles': function (ctx) {
+        return {
+            css: this.controls.output
         }
     }
 }
