4f64322e4f43ac4d5eaffcf1be86a0572cfb02b5
Piotr Fatyga <ADC0BGQ@IPMS.COM>
2017-01-24 10:53:57 -0500

    Fix for IE

--- public/src/app/app.js
+++ public/src/app/app.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 import mainModule from './main/mainModule'; // always run main module after others
 import './scalejs.extensions'; // setup extensions before running main module
 
--- public/src/appOutput/appOutput.js
+++ public/src/appOutput/appOutput.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 import 'appOutput/modules';
 import mainOutputModule from './mainOutput/mainOutputModule'; // always run main module after others
 
